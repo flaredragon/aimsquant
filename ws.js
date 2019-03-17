5c71ce08011ddc7e81baa9b8721ba49b0cf49df7
Akashdeep Ramnaney <akashdeepramnaney@gmail.com>
2019-03-17 06:23:36 +0530

    Refactor Websocket File

--- ws.js
+++ ws.js
@@ -1,22 +1,18 @@
 var axios = require('axios');
 var WebSocketServer = require('ws').Server,
-  wss = new WebSocketServer({port: 40510})
-var arr = [];
-var createdDate;
+wss = new WebSocketServer({port: 40510})
 
 wss.on('connection', function (ws) {
   ws.on('message', function (message) {
     console.log('received: %s', message)
+	var arr = [];
+	var createdDate;
 	axios.get('https://www.quandl.com/api/v3/datasets/NSE/'+message+'.json?api_key=MX4zkypoSjUzp8CyotQg')
   		.then(function (response) {
-	
 			arr.push(message);
-    //console.log(JSON.stringify(response.data));
 			var data = response.data.dataset.data;
 			var openarr = [],higharr = [],lowarr = [],lastarr = [],closearr =[] ,ttqarr = [],turnoverarr = [],dates = [];
-
 			data.forEach(function(element) {
-				//console.log(element[1]);
 				dates.push(element[0]);
 				openarr.push(element[1]);
 				higharr.push(element[2]);
@@ -25,38 +21,33 @@ wss.on('connection', function (ws) {
 				closearr.push(element[5]);
 				ttqarr.push(element[6]);
 				turnoverarr.push(element[7]);
-
 			});
 			var tickers =response.data.dataset.dataset_code;
-			 if(arr.filter(i => i === message).length==1)
+			if(arr.filter(i => i === message).length==1)
 				 createdDate = Date.now();
 			var updatedDate = Date.now();
 			var obj = {
 				ticker:tickers,
 				priceHistory:{
-					Dates:dates,
-					Open:openarr,
-					High:higharr,
-					Low:lowarr,
-					Last:lastarr,
-					Close:closearr,
-					TTQ:ttqarr,
-					Turnover:turnoverarr
-						},
+						Dates:dates,
+						Open:openarr,
+						High:higharr,
+						Low:lowarr,
+						Last:lastarr,
+						Close:closearr,
+						TTQ:ttqarr,
+						Turnover:turnoverarr
+					      },
 				createdDate:createdDate,
 				updatedDate:updatedDate
-				  }
+			}
 			return obj;
-			})
-			.then(function (obj) {
-				ws.send(JSON.stringify(obj));  
-			})
-			.catch(function (error) {
-			 	console.log(error);
-			  });  
-		})	
-})
-
-
-
-
+		})
+		.then(function (obj) {
+			ws.send(JSON.stringify(obj));  
+		})
+		.catch(function (error) {
+		 	console.log(error);
+		})  
+	})	
+});
