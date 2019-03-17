d600712b04f1ac91c7ae5a462903b691babfadeb
Akashdeep Ramnaney <akashdeepramnaney@gmail.com>
2019-03-17 06:19:48 +0530

    Refactored server.js

--- server.js
+++ server.js
@@ -7,9 +7,8 @@ var express = require('express'),
     axios = require('axios'),
     mongoose = require('mongoose'),
     Model = require('./models'),
-    {Stock} = Model,
+    { Stock } = Model,
     app = express();
-
 
 app.use(bodyParser.json());
 
@@ -18,7 +17,6 @@ mongoose.connect("mongodb://akash:akash@ds121599.mlab.com:21599/stock")
         ()=>{console.log('connected');},
         err =>{console.log('Error Connecting to DataBase');}
 );
-
 
 const schema = {
     query: {
@@ -33,74 +31,66 @@ var ws2 = new WebSocket('ws://localhost:40510');
 
 
 app.get('/stock' ,expressJoi(schema), function (req, res, next) {
-
 	var dt = req.query.dataType;
 	var finalarr = [];
 	Stock.findOne({ticker:req.query.ticker})
-      		.exec((err, products) => { console.log(products)
-
+      	     .exec((err, products) => {
 	if(products==null)
 	{
 		ws2.send(req.query.ticker);
 		ws2.on('message', function (message) {
 			var msg = JSON.parse(message);
-		Stock.findOneAndUpdate({
-   			ticker:req.query.ticker},
-			msg, {upsert: true, 'new': true}, function(err, res) {
-    				console.log(res);})
-		.then(function(response) {
-			var sp=0,ep=response.priceHistory['Dates'].length-1;
-
-			for(var i=0;i<response.priceHistory['Dates'].length;i++)
-			{
-        if(req.query.startDate)
-				{if(response.priceHistory['Dates'][i]==req.query.startDate)
-					sp=i;
-        }
-
-        if(req.query.endDate)
-        {
-				if(response.priceHistory['Dates'][i]==req.query.startDate)
-					{
-					ep=i;
-					break;
+			Stock.findOneAndUpdate({
+				ticker:req.query.ticker},
+				msg, {upsert: true, 'new': true}, function(err, res) {
+					console.log(res);})
+			       .then(function(response) {
+				var sp=0,ep=response.priceHistory['Dates'].length-1;
+				for(var i=0;i<response.priceHistory['Dates'].length;i++)
+				{
+					if(req.query.startDate)
+					{	
+						if(response.priceHistory['Dates'][i]==req.query.startDate)
+							sp=i;
 					}
+					if(req.query.endDate)
+					{
+						if(response.priceHistory['Dates'][i]==req.query.startDate)
+						{
+							ep=i;
+							break;
+						}
+				}
 			}
-    }
 			res.json(response.priceHistory[dt].slice(sp,ep+1));
-				console.log('Done!');
-		});
-
-	})
-}
-	else
-	{ var sp=0,ep=products.priceHistory['Dates'].length-1;
-
-  for(var i=0;i<products.priceHistory['Dates'].length;i++)
-  {
-    if(req.query.startDate)
-    {if(products.priceHistory['Dates'][i]==req.query.startDate)
-      sp=i;
-    }
-
-    if(req.query.endDate)
-    {
-    if(products.priceHistory['Dates'][i]==req.query.startDate)
-      {
-      ep=i;
-      break;
-      }
-  }
-}
-  res.json(products.priceHistory[dt].slice(sp,ep+1));
+			console.log('Done!');
+			})
+    	    	})
 	}
-
-     });
-
-})
-
-
-
+	else
+	{ 
+		var sp=0,ep=products.priceHistory['Dates'].length-1;
+  		for(var i=0;i<products.priceHistory['Dates'].length;i++)
+  		{
+    			if(req.query.startDate)
+    			{	
+				if(products.priceHistory['Dates'][i]==req.query.startDate)
+      					sp=i;
+    			}
+    			
+			if(req.query.endDate)
+    			{
+			    if(products.priceHistory['Dates'][i]==req.query.startDate)
+			    {
+			     	 ep=i;
+			      	 break;
+			     }
+			}
+		}
+  		res.json(products.priceHistory[dt].slice(sp,ep+1));
+	}
+     })
+});
 
 app.use(function (err, req, res, next) {
     if (err.isBoom) {
@@ -110,4 +100,4 @@ app.use(function (err, req, res, next) {
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
-})
+});
